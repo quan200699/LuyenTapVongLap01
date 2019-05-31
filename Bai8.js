eba3f5ebd627d6959c1e0b1160f9d682aa751c55
quan200699 <51079189+quan200699@users.noreply.github.com>
2019-05-31 10:25:41 +0700

    .

---
+++ Bai8.js
@@ -0,0 +1,27 @@
+function Play() {
+    let a = parseInt(prompt("Nhập điểm đầu:"));
+    let b = parseInt(prompt("Nhập điểm kết:"));
+    let  number = parseInt(prompt("Nhập số bạn đoán:"));
+    let life = 3;
+    let randomnumber = Math.round(Math.random()*(b-a)+a);
+    for(var i = a;i<=b;i++)
+    {
+        if(life==0)
+        {
+            alert("You Loose!Play Again!");
+            break;
+        }
+        if(randomnumber==number)
+        {
+            alert("Congratualtion!");
+            break;
+        }
+        else
+        {
+            alert("Wrong!");
+            life--;
+            alert("Please try again! You have "+life+" life");
+            number = parseInt(prompt("Nhập số bạn đoán:"));
+        }
+    }
+}
